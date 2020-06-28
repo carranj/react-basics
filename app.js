3fc75c9b07a8dc67b38f5d77d71d2b49412999eb
carranj <carranco.jose.r@gmail.com>
2020-06-28 13:38:52 -0500

    Create the Player Component

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const Header = () => (
-    <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
-    </header>
-)
+const Header = () => {
+    return(
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
+
+const Counter = () => {
+    return(
+        <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+    )
+    
+}
+
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Guil
+            </span>
+
+        <Counter />
+            
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <Header />,
+    <Player />,
     document.getElementById("root")
 );
